98c23d3f9271f7ad73379725c35636287936a9fe
Wuj94 <g.calabrese37@studenti.uniba.it>
2021-07-21 20:37:58 +0100

    PlayerRecord refactoring - lombok makes it easier to read/maintain

--- src/main/java/com/example/tictactoe/repository/config/PlayerRecord.java
+++ src/main/java/com/example/tictactoe/repository/config/PlayerRecord.java
@@ -1,18 +1,31 @@
 package com.example.tictactoe.repository.config;
 
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondarySortKey;
 
-//@Lombok
 @DynamoDbBean
+@AllArgsConstructor
+@Data
 public class PlayerRecord {
+
+    @Getter(onMethod_= {@DynamoDbPartitionKey})
     private UUID id;
+
+    @Getter(onMethod_= {@DynamoDbSecondaryPartitionKey(indexNames = {BETWEEN_RANKS_INDEX})})
     private String game;
+
+    @Getter(onMethod_= {@DynamoDbSecondarySortKey(indexNames = {BETWEEN_RANKS_INDEX})})
     private int pos;
+
     private String username;
+
+
     private int score;
 
     public static final String BETWEEN_RANKS_INDEX = "between_ranks_index";
@@ -20,32 +33,4 @@ public class PlayerRecord {
     public PlayerRecord() {
         this.pos = -1;
     }
-
-    public PlayerRecord(UUID id, String game, int pos, String username, int score) {
-        this.id = id;
-        this.pos = pos;
-        this.username = username;
-        this.score = score;
-        this.game = game;
-    }
-
-    @DynamoDbPartitionKey
-    public UUID getId() { return this.id; }
-    public void setId(UUID id) { this.id = id; }
-
-    // GSI named 'between_ranks_index'
-    @DynamoDbSecondarySortKey(indexNames = {BETWEEN_RANKS_INDEX})
-    public int getPos() { return this.pos; }
-    public void setPos(int pos) { this.pos = pos; }
-
-    @DynamoDbSecondaryPartitionKey(indexNames = {BETWEEN_RANKS_INDEX})
-    public String getGame() { return this.game; }
-    public void setGame(String game) { this.game = game; }
-    //
-
-    public String getUsername() { return this.username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public int getScore() { return this.score; }
-    public void setScore(int score) { this.score = score; }
 }
